ef28653112f5b44007b1c011228792b8637ee72e
Danil Grebnev <grebnevdanil60@gmail.com>
2023-04-25 16:42:22 +0300

    add style for scrollbar

--- src/app/components/pages/Main/index.tsx
+++ src/app/components/pages/Main/index.tsx
@@ -24,7 +24,7 @@ export const Main = () => {
             <CareerOpportunities />
             <Admission />
             <EquipmentGallery />
-            <UniversityInfo />
+            {/* <UniversityInfo /> */}
             <Partners />
             <DepartmentSpecialists />
             <Questions />
